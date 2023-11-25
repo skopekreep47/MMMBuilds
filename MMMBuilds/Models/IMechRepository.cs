5ae99ca2c4a11b285049051eeb9f60879c4371a5
skopekreep47 <mmmadrid47@gmail.com>
2023-11-24 21:01:25 -0700

    Added additional scaffolding for modularity in views. Added jQuery and Bootstrap with LibMan.

--- MMMBuilds/Models/IMechRepository.cs
+++ MMMBuilds/Models/IMechRepository.cs
@@ -3,7 +3,7 @@
     public interface IMechRepository
     {
         IEnumerable<Mechanism> AllMechs { get; }
-        IEnumerable<Mechanism> MechsOfTheWeek { get;}
+        IEnumerable<Mechanism> MechsOfTheWeek { get; }
 
         Mechanism? GetMechById(int mechId);
     }
--- MMMBuilds/Models/MockMechRepository.cs
+++ MMMBuilds/Models/MockMechRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Pipelines;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MMMBuilds.Models
 {
@@ -9,10 +10,10 @@ namespace MMMBuilds.Models
         public IEnumerable<Mechanism> AllMechs =>
             new List<Mechanism>
             {
-                new Mechanism {MechId = 1, Name="Schmidt Coupling", Price=115.95M, ShortDescription="Lorem Ipsum", LongDescription="The equations of translational kinematics can easily be extended to planar rotational kinematics for constant angular acceleration with simple variable exchanges.", Category = _categoryRepository.AllCategories.ToList()[0],ImageUrl="", InStock=true, IsMechOfTheWeek=false, ImageThumbUrl=""},
-                new Mechanism {MechId = 2, Name="Geneva Drive", Price=118.95M, ShortDescription="Lorem Ipsum", LongDescription="Although position in space and velocity in space are both true vectors (in terms of their properties under rotation), as is angular velocity, angle itself is not a true vector.", Category = _categoryRepository.AllCategories.ToList()[1],ImageUrl="", InStock=true, IsMechOfTheWeek=false, ImageThumbUrl=""},
-                new Mechanism {MechId = 3, Name="Scotch Yoke", Price=115.95M, ShortDescription="Lorem Ipsum", LongDescription="Important formulas in kinematics define the velocity and acceleration of points in a moving body as they trace trajectories in three-dimensional space.", Category = _categoryRepository.AllCategories.ToList()[0],ImageUrl="", InStock=true, IsMechOfTheWeek=true, ImageThumbUrl=""},
-                new Mechanism {MechId = 4, Name="Rack and Pinion", Price=112.95M, ShortDescription="Lorem Ipsum", LongDescription="In order to define these formulas, the movement of a component B of a mechanical system is defined by the set of rotations.", Category = _categoryRepository.AllCategories.ToList()[2],ImageUrl="", InStock=true, IsMechOfTheWeek=true, ImageThumbUrl=""}
+                new Mechanism {MechId = 1, Name="Schmidt Coupling", Price=115.95M, ShortDescription="Lorem Ipsum", LongDescription="The equations of translational kinematics can easily be extended to planar rotational kinematics for constant angular acceleration with simple variable exchanges.", Category = _categoryRepository.AllCategories.ToList()[0],ImageUrl="images/mech_schmidt.jpg", InStock=true, IsMechOfTheWeek=false, ImageThumbUrl="images/mech_schmidt.jpg"},
+                new Mechanism {MechId = 2, Name="Geneva Drive", Price=118.95M, ShortDescription="Lorem Ipsum", LongDescription="Although position in space and velocity in space are both true vectors (in terms of their properties under rotation), as is angular velocity, angle itself is not a true vector.", Category = _categoryRepository.AllCategories.ToList()[1],ImageUrl="images/mech_geneva.jpg", InStock=true, IsMechOfTheWeek=false, ImageThumbUrl="images/mech_geneva.jpg"},
+                new Mechanism {MechId = 3, Name="Scotch Yoke", Price=115.95M, ShortDescription="Lorem Ipsum", LongDescription="Important formulas in kinematics define the velocity and acceleration of points in a moving body as they trace trajectories in three-dimensional space.", Category = _categoryRepository.AllCategories.ToList()[0],ImageUrl="images/mech_scotch.jpg", InStock=true, IsMechOfTheWeek=true, ImageThumbUrl="images/mech_scotch.jpg"},
+                new Mechanism {MechId = 4, Name="Rack and Pinion", Price=112.95M, ShortDescription="Lorem Ipsum", LongDescription="In order to define these formulas, the movement of a component B of a mechanical system is defined by the set of rotations.", Category = _categoryRepository.AllCategories.ToList()[2],ImageUrl="images/mech_rack.jpg", InStock=true, IsMechOfTheWeek=true, ImageThumbUrl="images/mech_rack.jpg"}
             };
 
         public IEnumerable<Mechanism> MechsOfTheWeek
@@ -23,7 +24,7 @@ namespace MMMBuilds.Models
             }
         }
 
-        public Mechanism? GetMechById(int pieId) => AllMechs.FirstOrDefault(p => p.MechId == pieId);
+        public Mechanism? GetMechById(int mechId) => AllMechs.FirstOrDefault(p => p.MechId == mechId);
 
         public IEnumerable<Mechanism> SearchMechs(string searchQuery)
         {
